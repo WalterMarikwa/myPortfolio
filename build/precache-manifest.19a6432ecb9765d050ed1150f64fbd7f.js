b349bcdccdebd61c5d7d968fee18574c6d78c4db
Walter Marikwa <waltermarikwa02@gmail.com>
2023-08-29 23:36:47 -0400

    Updated files

---
+++ build/precache-manifest.19a6432ecb9765d050ed1150f64fbd7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dde855f419be6ff9b4c1b10e95166de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d921b876b5c9bf9f360",
+    "url": "/static/css/main.f3565fcc.chunk.css"
+  },
+  {
+    "revision": "508c11fd7833f3330003",
+    "url": "/static/js/2.35f27d22.chunk.js"
+  },
+  {
+    "revision": "1d921b876b5c9bf9f360",
+    "url": "/static/js/main.8ab1088d.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "12f98a5de1f8ae400d404b9b0a96d7d0",
+    "url": "/static/media/walterv2.12f98a5d.jpg"
+  }
+]);
